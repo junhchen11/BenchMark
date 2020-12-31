d19ed8b499f9dc187356ebe35b69baeca2e09ba0
junhchen11 <junhchen11@gmail.com>
2020-12-31 10:24:21 -0600

    Lift Form and Lift Card components added

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './Styles';
-import DropDown from './DropDown';
 
+import LiftCard from './Containers/LiftCard';
+liftStats = {
+	sets: '3',
+	reps: '8',
+	weight: '315',
+	lift: 'Bench',
+	date: '10/30/20',
+	curMax: '415',
+};
 export default function App() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.Container}>
+			<LiftCard liftStats={liftStats} editable={true} />
+		</View>
+	);
 }
